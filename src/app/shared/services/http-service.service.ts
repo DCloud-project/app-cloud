d470022a82d027a9bba2ecf54f2f1edd2d85ec42
leijing000 <1084961608@qq.com>
2020-05-22 22:51:16 +0800

    请求

--- src/app/shared/services/http-service.service.ts
+++ src/app/shared/services/http-service.service.ts
@@ -8,35 +8,52 @@ import Qs from 'qs';
 })
 export class HttpServiceService {
 
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient) {
+  }
+
   //axios方法
-  commonUrl='http://localhost:8080';
-  // commonUrl="http://edatyp.natappfree.cc";
+  // commonUrl = 'http://7vtbvj.natappfree.cc';
+  commonUrl = 'http://localhost:8080';
+  // instance = axios.create({
+  //   headers: { 'token': localStorage.getItem("token") }
+  // });
   //获取
   get(api, params) {
     return new Promise((resolve, reject) => {
-      axios.get(this.commonUrl+api, {
+      this.setToken();
+      axios.interceptors.request.use((config) => {
+        if (localStorage.getItem("token")) {
+          config.headers['token']=localStorage.getItem("token");
+        }
+        console.log(config.headers);
+        return config;
+      },(error) =>{
+        console.log('错误参数')
+        return Promise.reject(error);
+      });
+      axios.get(this.commonUrl + api, {
         params: params,
-    }).then(function (response) {
-            resolve(response);
-          })
-          .catch(function (error) {
-            reject(error);
-          })
+      }).then(function (response) {
+        resolve(response);
+      })
+        .catch(function (error) {
+          reject(error);
+        })
     })
-}
+  }
   //获取（不携带参数）
   getAll(api) {
     return new Promise((resolve, reject) => {
+      this.setToken();
       axios({
         headers: {
           'Content-Type': 'application/json'
         },
         method: 'get',
-        url: this.commonUrl+api
-    }).then(function (response) {
-          resolve(response);
-        })
+        url: this.commonUrl + api
+      }).then(function (response) {
+        resolve(response);
+      })
         .catch(function (error) {
           reject(error);
         })
@@ -45,16 +62,30 @@ export class HttpServiceService {
   //新增
   post(api, params) {
     return new Promise((resolve, reject) => {
+      this.setToken();
       axios({
-        headers: {
-            'Content-Type': 'application/json'
-        },
         method: 'post',
-        url: this.commonUrl+api,
-        data: params
-    }).then(function (response) {
-          resolve(response);
-        })
+        url: this.commonUrl + api,
+        data: params,
+      }).then(function (response) {
+        resolve(response);
+      })
+        .catch(function (error) {
+          reject(error);
+        });
+    })
+  }
+  put(api, params) {
+    this.setToken();
+    return new Promise((resolve, reject) => {
+      axios({
+        method: 'put',
+        url: this.commonUrl + api,
+        data: params,
+      }).then(function (response) {
+        console.log(response);
+        resolve(response);
+      })
         .catch(function (error) {
           reject(error);
         });
@@ -63,38 +94,60 @@ export class HttpServiceService {
   //编辑
   patch(api, params) {
     return new Promise((resolve, reject) => {
+      this.setToken();
       axios({
         headers: {
-            'Content-Type': 'application/json'
+          'Content-Type': 'application/json'
         },
         method: 'patch',
-        url: this.commonUrl+api,
+        url: this.commonUrl + api,
         data: params
-    }).then(function (response) {
-          resolve(response);
-        })
+      }).then(function (response) {
+        resolve(response);
+      })
         .catch(function (error) {
           reject(error);
         });
     })
   }
-//   //删除
+  //   //删除
   delete(api, params) {
-      return new Promise((resolve, reject) => {
-        axios({
-          headers: {
-            'Content-Type': 'application/json'
+    return new Promise((resolve, reject) => {
+      this.setToken();
+      axios({
+        headers: {
+          'Content-Type': 'application/json'
         },
-          method: 'delete',
-          url: this.commonUrl+api,
-          data: params
+        method: 'delete',
+        url: this.commonUrl + api,
+        data: params
       }).then(function (response) {
-            resolve(response);
-          })
-          .catch(function (error) {
-            reject(error);
-          })
+        resolve(response);
       })
+        .catch(function (error) {
+          reject(error);
+        })
+    })
   }
-  
+  //设置同步
+  runAfter(function1,function2){
+    axios.all([function1])
+    .then(axios.spread(function2))
+  }
+  setToken(){
+    //拦截器 头部设置token
+    axios.interceptors.request.use((config) => {
+      // this.setToken();
+     if (localStorage.getItem("token")) {
+       config.headers['token']=localStorage.getItem("token");
+     }
+     console.log(config.headers);
+     console.log(config);
+     return config;
+   },(error) =>{
+     console.log('错误参数')
+     return Promise.reject(error);
+   });
+ }
+
 }
