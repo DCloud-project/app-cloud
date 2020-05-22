8525344edfa24db206cc8f99e7a4edb05ea86f0a
leijing000 <1084961608@qq.com>
2020-05-22 22:48:44 +0800

    确认加入

---
+++ src/app/layout/joinByCode/confirm-join/confirm-join.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-join',
+  templateUrl: './confirm-join.page.html',
+  styleUrls: ['./confirm-join.page.scss'],
+})
+export class ConfirmJoinPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
