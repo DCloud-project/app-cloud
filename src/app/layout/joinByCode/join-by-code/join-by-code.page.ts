20b4640e9e5a5366f5e5a1b6c88dea3c6cd4bcb2
leijing000 <1084961608@qq.com>
2020-06-05 23:38:25 +0800

    班课号加入

--- src/app/layout/joinByCode/join-by-code/join-by-code.page.ts
+++ src/app/layout/joinByCode/join-by-code/join-by-code.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/compiler/src/core';
+import { Router } from '@angular/router';
+import { HttpServiceService } from 'src/app/shared/services/http-service.service';
+import { HttpClient } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-join-by-code',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoinByCodePage implements OnInit {
 
-  constructor() { }
+  public code = '';
+  constructor(
+    private router: Router,
+    public httpService: HttpServiceService,
+    public http: HttpClient,
+    private alertController: AlertController,
+  ) { }
 
   ngOnInit() {
   }
 
+  joinLesson() {
+    var params = {
+      // email: localStorage.getItem("email"),
+      code: this.code
+    }
+    var api = '/courses';//后台接口
+    this.httpService.post(api, params).then(async (response: any) => {
+      if (response.status == 200) {
+        if (response.data.respCode == "班课号不存在！") {
+          const alert = await this.alertController.create({
+            header: '警告',
+            message: '班课号不存在！',
+            buttons: ['确认']
+          });
+          await alert.present();
+        } else {
+          //获得班课信息
+          localStorage.setItem("joinCode",this.code); 
+          localStorage.setItem("joinInf",JSON.stringify(response.data)); 
+          this.router.navigateByUrl("/confirm-join");
+        }
+      }
+
+    })
+
+
+  }
 }
